a31b8942aa2ef3c8ea15ffa3a39951a2eef88658
Afeng99886 <afeng99886@126.com>
2021-04-12 17:40:33 +0800

    完成了后台主页换头像和退出功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,26 @@
 // 在这个函数中 我们可以拿到Ajax({})中的所有配置对象/选项
 $.ajaxPrefilter(function (options) {
    options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+
+   // 统一为有权限的接口，设置 headers 请求头
+   if (options.url.indexOf('/my/') !== -1) {
+      options.headers = {
+         Authorization: localStorage.getItem('token') || ''
+      }
+
+      // 优化权限控制的代码   
+      // 控制用户的访问权限,除了登录注册页面，其他的页面都需要控制用户访问其他页面的权利，
+      // 所以在每次发送ajax请求的时候，先调用ajaxprefilter()函数，预过滤
+      // 不论成功还是失败，最终都会调用 complete 回调函数
+      options.complete = function (res) {
+         // console.log(res);
+         // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+         if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 1.强制(清空)本地存储的token，  以防假token
+            localStorage.removeItem('token');
+            // 2.强制跳转到登录页面
+            location.href = '/login.html';
+         }
+      }
+   }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,73 @@
+$(function () {
+   getUserInfo();
+})
+
+// 封装获取用户基本信息  getUserInfo 函数
+function getUserInfo() {
+   $.ajax({
+      method: 'GET',
+      url: '/my/userinfo',
+      // headers是请求头配置对象
+      // headers: {
+      //    Authorization: localStorage.getItem('token') || ''
+      // },
+      success: function (res) {
+         // console.log(res);
+         if (res.status !== 0) {
+            return layui.layer.msg('获取用户基本信息失败！');
+         }
+
+         // 调用 renderAvatar 获取用户头像
+         renderAvatar(res.data);
+      },
+      //  控制用户的访问权限,除了登录注册页面，其他的页面都需要控制用户访问其他页面的权利，
+      // 所以在每次发送ajax请求的时候，先调用ajaxprefilter()函数，预过滤
+      // 不论成功还是失败，最终都会调用 complete 回调函数
+      /* complete: function (res) {
+         // console.log(res);
+         // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+         if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 1.强制(清空)本地存储的token，  以防假token
+            localStorage.removeItem('token');
+            // 2.强制跳转到登录页面
+            location.href = '/login.html';
+         }
+      } */
+   })
+}
+
+
+// 渲染用户头像/用户名
+function renderAvatar(userData) {
+   // 1.获取用户名
+   var name = userData.nickname || userData.username;
+   // 2.设置欢迎文本
+   $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+   // 按需渲染用户的头像
+   if (userData.user_pic !== null) {
+      // 设置用户图片头像
+      $('.layui-nav-img').attr('src', userData.user_pic).show();
+      $('.text-avatar').hide();
+   } else {
+      // 设置文本头像
+      $('.layui-nav-img').hide();
+      // console.log(name[0]);  // w
+      var first = name[0].toUpperCase();
+      $('.text-avatar').html(first).show();
+
+   }
+}
+
+//实现退出功能
+// 监听点击事件
+$('#btnLogout').on('click', function () {
+   layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+      // 1.删除(清空)用户登录时在本地存储的token
+      localStorage.removeItem('token');
+      // 2.跳转到登录页面
+      location.href = '/login.html';
+
+      // 关闭confirm询问框
+      layer.close(index);
+   });
+})
